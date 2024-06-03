41f7e6dc720accc20a1ca8b0bf26259fa34915b4
Jethro Modequillo <jethro.modequillo@mensaphilippines.org>
2024-06-03 22:37:16 +0800

    add initial Appointment model

--- technical_assessment/appointments/models.py
+++ technical_assessment/appointments/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+
 # Create your models here.
+class Appointment(models.Model):
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    patients = models.CharField(max_length=30)
+    comments = models.TextField()
+
+    def __str__(self):
+        return self.start_time
