41c4e573d551c97df3efb71f143850f73b4371ce
Jethro Modequillo <jethro.modequillo@mensaphilippines.org>
2024-06-04 15:15:39 +0800

    block PUT if appointment will cause overbooking

--- technical_assessment/appointments/views.py
+++ technical_assessment/appointments/views.py
@@ -18,8 +18,22 @@ class AppointmentViewSet(viewsets.ModelViewSet):
 
         return super().create(request, *args, **kwargs)
 
-    def check_for_conflicts(self, start_time, end_time):
+    def update(self, request, *args, **kwargs):
+        start_time = request.data.get("start_time")
+        end_time = request.data.get("end_time")
+        instance = self.get_object()
+
+        if self.check_for_conflicts(start_time, end_time, instance.id):
+            raise exceptions.ValidationError(
+                "The appointment time conflicts with an existing appointment."
+            )
+
+        return super().update(request, *args, **kwargs)
+
+    def check_for_conflicts(self, start_time, end_time, instance_id=None):
         conflicting_appointments = Appointment.objects.filter(
             start_time__lt=end_time, end_time__gt=start_time
         )
+        if instance_id:
+            conflicting_appointments = conflicting_appointments.exclude(id=instance_id)
         return conflicting_appointments.exists()
