740aeec49a6d108fe72fe52deb639d78e02c81a4
Jethro Modequillo <jethro.modequillo@mensaphilippines.org>
2024-06-07 14:16:20 +0800

    represent patients *only* by their names, no ID

--- technical_assessment/appointments/serializers.py
+++ technical_assessment/appointments/serializers.py
@@ -1,13 +1,14 @@
 from rest_framework import serializers
+
 from appointments.models import Appointment
 from patients.models import Patient
-from patients.serializers import PatientSerializer
 
 
 class AppointmentSerializer(serializers.ModelSerializer):
-    patients = PatientSerializer(many=True, read_only=True)
-    patient_ids = serializers.PrimaryKeyRelatedField(
-        queryset=Patient.objects.all(), many=True, write_only=True
+    patients = serializers.SlugRelatedField(
+        many=True,
+        slug_field="name",
+        queryset=Patient.objects.all(),
     )
 
     class Meta:
@@ -17,18 +18,5 @@ class AppointmentSerializer(serializers.ModelSerializer):
             "start_time",
             "end_time",
             "patients",
-            "patient_ids",
             "comments",
         )
-
-    def create(self, validated_data):
-        patient_ids = validated_data.pop("patient_ids")
-        appointment = Appointment.objects.create(**validated_data)
-        appointment.patients.set(patient_ids)
-        return appointment
-
-    def update(self, instance, validated_data):
-        patient_ids = validated_data.pop("patient_ids", None)
-        if patient_ids is not None:
-            instance.patients.set(patient_ids)
-        return super().update(instance, validated_data)
