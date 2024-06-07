026ae2c21984b4235279b489105a7a58c477d07e
Jethro Modequillo <jethro.modequillo@mensaphilippines.org>
2024-06-07 12:36:13 +0800

    set up patients ModelViewSet

---
+++ technical_assessment/patients/serializers.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+
+from patients.models import Patient
+
+
+class PatientSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Patient
+        fields = (
+            "id",
+            "name",
+        )
